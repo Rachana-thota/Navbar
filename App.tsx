742c999362f58fcd7b6d2bb08e237553a516607e
Rachana-thota <thotarachana29@gmail.com>
2024-12-20 15:08:01 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './componenets/NavBar'
+import Home from './componenets/Home'
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      
+    </div>
+  )
+}
